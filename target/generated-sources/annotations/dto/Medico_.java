a31c8eb97e8e9c14a0fe81fbd9f1c5439003517f
Hatsumy <hatsumyvb@gmail.com>
2025-06-02 19:40:05 -0500

    por si acaso

--- target/generated-sources/annotations/dto/Medico_.java
+++ target/generated-sources/annotations/dto/Medico_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2025-06-02T19:28:52")
+@Generated(value="EclipseLink-2.7.9.v20210604-rNA", date="2025-06-02T19:39:09")
 @StaticMetamodel(Medico.class)
 public class Medico_ { 
 
